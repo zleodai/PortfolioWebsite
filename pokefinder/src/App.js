89336c69c170604cbb54b10556e06e021c6f1572
zleodai <awsomeleodai@gmail.com>
2023-09-26 04:36:07 -0700

    start of many commmits
    Added some basic structure but alot needs to be worked on and polished ...

--- pokefinder/src/App.js
+++ pokefinder/src/App.js
@@ -7,14 +7,70 @@ function App() {
         <div id = "headerName">
           Leo Dai
         </div>
-        <div id = "contactIcons">
-          A, B, C, D
+        <div id = "buttonContainer">
+          <button className = "Buttons" id = "education">
+            Education
+          </button>
+          <button className = "Buttons" id = "projects">
+            Projects
+          </button>
+          <button className = "Buttons" id = "experience">
+            Work Experience
+          </button>
+          <button className = "Buttons" id = "skills">
+            Skills
+          </button>
+          <button className = "Buttons" id = "resume">
+            Resume
+          </button>
+          <button className = "Buttons" id = "contact">
+            Contact
+          </button>
         </div>
       </header>
       <div className="Front">
-        <p id="testAnimation">
-          this should spin
-        </p>
+        <div className = "Education">
+          <img src = {require('./media/lmuPicture.jpg')} id = "lmuPicture" alt = "LMU Photo"/>
+          <div className = "TextContainer">
+            <div id = "info">
+              Loyola Marymount University, <a href="https://www.lmu.edu/">lmu.edu</a>
+            </div>
+            <div id = "relevance">
+              Currently pursuing an undergraduate degree in computer science
+            </div>
+            <div id = "relevance1">
+              Graduating 2025 June
+            </div>
+            <div id = "coursesHeader">
+                Courses taken:
+              </div>
+            <div className = "Courses">
+              <div id = "0">Data Structures and Applications</div>
+              <div id = "1">Algorithmns and Analysis</div>
+              <div id = "2">Data Science</div>
+              <div id = "3">Game Design</div>
+              <div id = "4">Web App Development</div>
+              <div id = "5">Artificial Intelligence</div>
+              <div id = "6">Computer Systems Organization</div>
+              <div id = "7">Game Development</div>
+            </div>
+          </div>
+        </div>
+        <div className = "Projects">
+    
+        </div>
+        <div className = "Experience">
+
+        </div>
+        <div className = "Skills">
+
+        </div>
+        <div className = "Resume">
+
+        </div>
+        <div className = "Contact">
+
+        </div>
       </div>
     </div>
   );
