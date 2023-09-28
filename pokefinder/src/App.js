56f0e0915b4cc9103938556edc03f4603a1e9b93
zleodai <awsomeleodai@gmail.com>
2023-09-28 01:58:07 -0700

    finished work experiennce overal layout
    now u just gotta make it look good

--- pokefinder/src/App.js
+++ pokefinder/src/App.js
@@ -35,9 +35,7 @@ function App() {
         </div>
         <div className = "Education">
           <div className = "Padding" id = "educationAPadding"></div>
-          
           <img src = {require('./media/lmuPicture.jpg')} id = "lmuPicture" alt = "LMU Photo"/>
-
           <div className = "TextContainer">
             <div id = "info">
               Loyola Marymount University 
@@ -46,7 +44,7 @@ function App() {
               Located at 1 Loyola Marymount University Dr, Los Angeles, CA 90045
             </div>
             <div id = "relevance1">
-              I am currently studying at the College of Science and Engineering. I will be graduating in 2025 with an Undergraduate in Computer Science. I also intend on going for a Masters in Computer Science.
+              I am currently studying at the College of Science and Engineering. I will be graduating in 2025 with an Undergraduate in Computer Science. I also intend on going for a master's degree in Computer Science.
             </div>
             <div id = "buttonContainer">
               <a href = "https://lmu.edu">
@@ -57,18 +55,34 @@ function App() {
             </div>
           </div>
           <div className = "Padding" id = "educationBPadding"></div>
-
-
         </div>
         <div className = "ExperienceBanner">
           <div className = "Sides" id = "leftContent"></div>
-            <div id = "content">
-              Work Experience
-            </div>
+            <div id = "content"></div>
           <div className = "Sides" id = "rightContent"></div>
         </div>
         <div className = "Experience">
-          <div id = "info"> 
+          <div className = "Top"> 
+            <div id = "logoContainer">
+              <img src = {require('./media/targetIcon.png')} id = "logo" alt = "Target Logo"/>
+            </div>
+            <div id = "title">Work Experience</div>
+            <div id = "filler"></div>
+          </div>
+          <div className = "Info">
+            <div id = "title">General Merchandise Expert</div>
+            <div id = "date">October 2021 - Present</div>
+          </div>
+          <div className = "Content">
+            <div id = "content">
+              - Provided customer service to all guests and gained experience in sales
+            </div>
+            <div id = "content1">
+              - Experience with deadlines and daily tasks as part of the work routine
+            </div>
+            <div id = "content2">
+              - Communicated with management when issues arose with deadlines or on-hand tasks
+            </div>
           </div>
         </div>
         <div className = "Projects">
